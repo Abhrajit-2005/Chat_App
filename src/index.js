10b41fb4783d7f9689a9fef7cb96639f202d26e5
Abhrajit Chatterjee <abhrajitc23@gmail.com>
2025-05-15 20:10:34 +0530

    Setup of basic websocket is done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
+const { log } = require('console');
 const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+
 const app = express();
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("A User got connected");
+
+    socket.on('from_client', () => {
+        console.log("Event coming from client");
+    })
+
+    setInterval(() => {
+        socket.emit('from_server');
+    }, 2000);
+});
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+server.listen(3000, () => {
+    console.log("Server Started");
 });
